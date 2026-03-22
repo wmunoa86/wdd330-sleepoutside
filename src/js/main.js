698089a42fe3de3ba5ac7aa160aaf12f7363d712
wmunoa86 <wmunoa@icloud.com>
2026-03-22 17:15:28 -0600

    week3 - team

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
 const myList = new ProductList("tents", dataSource, listElement);
-
 myList.init();
